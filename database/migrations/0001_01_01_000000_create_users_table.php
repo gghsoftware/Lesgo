bdb61fe877bf4c8f6729a2064cb568c95ac2f45f
NooisY <143975472+KARLOSSONRIVO@users.noreply.github.com>
2026-01-25 10:35:37 +0800

    added the role and phone_number to the schema

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone_number', 50)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('customer');
             $table->rememberToken();
             $table->timestamps();
         });
